307c8633b228057e49744f3d3c2b696cda55299b
Bryan <bryanlima200@outlook.com>
2021-08-02 00:41:33 -0300

    Implementado uso do uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 
@@ -20,19 +21,22 @@ const App = () => {
 	]);
 
 	const handleTaskAddition = (taskTitle) => {
-		const newTasks = [...tasks, {
-			title: taskTitle,
-			id: Math.random(10),
-			completed: false,
-		}];
+		const newTasks = [
+			...tasks,
+			{
+				title: taskTitle,
+				id: uuidv4(),
+				completed: false,
+			},
+		];
 
 		setTasks(newTasks);
-	}
+	};
 
 	return (
 		<>
 			<div className="container">
-				<AddTask handleTaskAddition={handleTaskAddition}/>
+				<AddTask handleTaskAddition={handleTaskAddition} />
 				<Tasks tasks={tasks} />
 			</div>
 		</>
